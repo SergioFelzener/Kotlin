673e5dc65969f96a907b4808cdd445580f623dea
Sergio Felzener <felzenerdj@gmail.com>
2020-10-02 22:33:01 -0300

    Start User Menu

--- NoteApp/app/src/main/java/br/senac/noteapp/activity/ListNoteActivity.kt
+++ NoteApp/app/src/main/java/br/senac/noteapp/activity/ListNoteActivity.kt
@@ -3,6 +3,9 @@ package br.senac.noteapp.activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import br.senac.noteapp.R
 import br.senac.noteapp.business.NotesSingleton
 import kotlinx.android.synthetic.main.activity_list_note.*
@@ -27,14 +30,35 @@ class ListNoteActivity : AppCompatActivity() {
 
     fun refreshNotes() {
         containerFab.removeAllViews()
-        
+
         for (note in NotesSingleton.listNotes) {
             val card = layoutInflater.inflate(R.layout.note_card, containerFab, false)
 
             card.txtTitle.text = note.title
             card.txtDesc.text = note.description
 
+            card.setOnClickListener{
+                Toast.makeText(this, note.title, Toast.LENGTH_LONG).show()
+            }
+
             containerFab.addView(card)
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        menuInflater.inflate(R.menu.user_menu, menu)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.user){
+            val intent = Intent(this, UserActivity::class.java)
+            startActivity(intent)
+        }
+
+
+        return super.onOptionsItemSelected(item)
     }
 }
